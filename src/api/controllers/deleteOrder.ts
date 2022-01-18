c90cafff67b58c9a8ba2b86207dbd2cdc1210ca5
Tomi_Ohl <ohlslager.tom@gmail.com>
2022-01-18 17:04:09 +0100

    Remove valid cycle check from deleting orders It wasn't implemented anyways and it's probably not even needed. Old bids are periodically deleted on the Solana version too. Here, while that's not needed, disallowing it doesn't bring advantages either. Since not having this saves us a contract call and decoding order parameters, this could count as an optimization too lol

--- src/api/controllers/deleteOrder.ts
+++ src/api/controllers/deleteOrder.ts
@@ -6,15 +6,10 @@ const deleteOrder: ControllerFunction = async (req, res) => {
   try {
     const { auctionId, order, signature } = req.body;
     if (verifySignature(order, signature)) {
-      let validCycle: boolean; // TODO: ensure that the order's cycle is the current or a future cycle
-      if (!validCycle)
-        res.status(500).json({ message: "The cycle is already expired, therefore the bid cannot be deleted" });
-      else {
-        const fileKey = createFileKey(auctionId, order.makerAddress);
-        const buckets = await listBuckets();
-        await deleteFile(fileKey, buckets[0].name);
-        res.status(200).json({ message: "Delete request sent" });
-      }
+      const fileKey = createFileKey(auctionId, order.makerAddress);
+      const buckets = await listBuckets();
+      await deleteFile(fileKey, buckets[0].name);
+      res.status(200).json({ message: "Delete request sent" });
     } else res.status(401).json({ message: "Invalid signature" });
   } catch (error) {
     if ((error as Error).message === "Incorrect signature")
