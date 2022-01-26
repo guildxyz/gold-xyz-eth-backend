01bcd4a47462a5d3faf000da725626eaee131236
Tomi_Ohl <ohlslager.tom@gmail.com>
2022-01-26 21:32:50 +0100

    Some error handling

--- src/api/controllers/closeCycle.ts
+++ src/api/controllers/closeCycle.ts
@@ -4,9 +4,12 @@ import closeAuctionCycle from "../../utils/closeAuctionCycle.js";
 const closeCycle: ControllerFunction = async (req, res) => {
   try {
     const { auctionId } = req.body;
-    if (await closeAuctionCycle(auctionId))
+    try {
+      await closeAuctionCycle(auctionId);
       res.status(200).json({ message: `Closing the current cycle of ${auctionId}` });
-    else res.status(500).json({ message: `Closing the current cycle of ${auctionId} failed` });
+    } catch (error: any) {
+      res.status(500).json({ message: `Closing the current cycle of ${auctionId} failed`, error: error.message });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Unexpected error" });
--- src/utils/closeAuctionCycle.ts
+++ src/utils/closeAuctionCycle.ts
@@ -3,12 +3,11 @@ import { goldContract } from "../config/contract.js";
 import getMaxBid from "./getMaxBid.js";
 import { orderInfo, protocolFee, isValidHashSignature, isValidOrderSignature } from "./ZeroExExchangeUtils.js";
 
-// TODO: implement better error handling
 const closeAuctionCycle = async (auctionId: string) => {
   try {
     const maxBid = await getMaxBid(auctionId);
 
-    if (maxBid === undefined) return false;
+    if (maxBid === undefined) throw new Error("Cannot get the winning bid");
 
     const orderHash = orderHashUtils.getOrderHash(maxBid.order);
     const hashSignature = await isValidHashSignature(orderHash, maxBid.order.makerAddress, maxBid.signature);
@@ -21,18 +20,17 @@ const closeAuctionCycle = async (auctionId: string) => {
           gasLimit: 2000000,
           value: fee,
         });
-      } catch (error) {
+      } catch (error: any) {
         console.error(error);
-        return false;
+        throw new Error("Auction cycle cannot be closed in the contract");
       }
       await goldContract.closeAuctionCycle(maxBid.order, maxBid.signature, auctionId, {
         value: fee,
       });
-      return true;
-    } else return false;
+    } else throw new Error("The winning bid is incorrect");
   } catch (e) {
     console.error(e);
-    return false;
+    throw e;
   }
 };
 
