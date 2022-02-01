aba8fbaf8e3d0d88e79906c9bc6ae91f1fa81426
Tomi_Ohl <ohlslager.tom@gmail.com>
2022-02-01 14:57:48 +0100

    Validate orders before saving them These are mostly the same validations as in the smart contract

--- src/api/controllers/saveOrder.ts
+++ src/api/controllers/saveOrder.ts
@@ -1,3 +1,5 @@
+import { assetDataUtils, ERC20AssetData, ERC721AssetData } from "@0x/order-utils";
+import { goldContract } from "../../config/contract.js";
 import { createFileKey, deleteFile, listBuckets, uploadFile } from "../../ipfs.js";
 import ControllerFunction from "../../types/ControllerFunction.js";
 import { ErrorWithCode, handleError } from "../../utils/errors.js";
@@ -14,6 +16,24 @@ const saveOrder: ControllerFunction = async (req, res) => {
     if (!auctions.includes(auctionId)) throw new ErrorWithCode(`Auction ${auctionId} does not exist`, 404);
 
     if (!(await isOrderFillable(order, signature))) throw new ErrorWithCode("The bid is incorrect", 500);
+
+    const auctionConfig = await goldContract.getAuctionConfig(auctionId);
+    const auctionStatus = await goldContract.getAuctionStatus(auctionId);
+    if (auctionConfig.minimumBidAmount.gt(order.makerAssetAmount)) throw new ErrorWithCode("Bid too low", 500);
+    if (auctionStatus.currentAuctionCycleStartTime.add(auctionConfig.cyclePeriod).lt(Math.ceil(Date.now() / 1000)))
+      throw new ErrorWithCode("Auction cycle ended", 500);
+    if (!auctionStatus.isActive) throw new ErrorWithCode("Auction is not active", 500);
+    if (auctionStatus.isFrozen) throw new ErrorWithCode("Auction is frozen", 500);
+
+    const bidderTokenAssetData = assetDataUtils.decodeAssetDataOrThrow(order.makerAssetData) as ERC20AssetData;
+    if (bidderTokenAssetData.tokenAddress !== auctionConfig.bidderTokenAddress.toLowerCase())
+      throw new ErrorWithCode("Bid with an incorrect token", 500);
+
+    const nftAssetData = assetDataUtils.decodeAssetDataOrThrow(order.takerAssetData) as ERC721AssetData;
+    if (nftAssetData.tokenAddress !== auctionConfig.nftAddress.toLowerCase())
+      throw new ErrorWithCode("Bid for a different NFT address", 500);
+    if (!auctionStatus.currentAuctionCycle.eq(Number(nftAssetData.tokenId.toString())))
+      throw new ErrorWithCode("Bid for a different NFT id", 500);
 
     const fileKey = createFileKey(auctionId, order.makerAddress);
     const buckets = await listBuckets();
