761d231c0dc502e4954b5367577627ebc0b70855
Tomi_Ohl <ohlslager.tom@gmail.com>
2022-02-01 18:59:58 +0100

    Use the default bucket when deleting files

--- src/api/controllers/deleteOrder.ts
+++ src/api/controllers/deleteOrder.ts
@@ -1,14 +1,13 @@
 import verifySignature from "../../utils/verifySignature.js";
 import ControllerFunction from "../../types/ControllerFunction.js";
-import { createFileKey, listBuckets, deleteFile } from "../../ipfs.js";
+import { createFileKey, deleteFile } from "../../ipfs.js";
 
 const deleteOrder: ControllerFunction = async (req, res) => {
   try {
     const { auctionId, order, signature } = req.body;
     if (verifySignature(order, signature)) {
       const fileKey = createFileKey(auctionId, order.makerAddress);
-      const buckets = await listBuckets();
-      await deleteFile(fileKey, buckets[0].name);
+      await deleteFile(fileKey);
       res.status(200).json({ message: "Delete request sent" });
     } else res.status(401).json({ message: "Invalid signature" });
   } catch (error) {
