c915db2e8700fb348ba7c74deeb715901425ecd8
Tomi_Ohl <ohlslager.tom@gmail.com>
2022-02-02 22:36:57 +0100

    Changed delete bid endpoint to allow deletion for the bidders

--- src/api/controllers/deleteOrder.ts
+++ src/api/controllers/deleteOrder.ts
@@ -1,16 +1,23 @@
-import { createFileKey, deleteFile } from "../../ipfs.js";
+import { createFileKey, deleteFile, getFile } from "../../ipfs.js";
 import ControllerFunction from "../../types/ControllerFunction.js";
 import { ErrorWithCode, handleError } from "../../utils/errors.js";
 import { verifySignature } from "../../utils/signatures.js";
 
 const deleteOrder: ControllerFunction = async (req, res) => {
   try {
-    const { auctionId, order, signature } = req.body;
-    if (verifySignature(order, signature)) {
-      const fileKey = createFileKey(auctionId, order.makerAddress);
-      await deleteFile(fileKey);
-      res.status(200).json({ message: "Delete request sent" });
-    } else throw new ErrorWithCode("Invalid signature", 403);
+    // signature: the order's salt is signed
+    const { auctionId, address, signature } = req.body;
+
+    const fileKey = createFileKey(auctionId, address);
+    const file = await getFile(fileKey);
+    if (file === undefined) throw new ErrorWithCode(`Address ${address} has no bids in auction ${auctionId}`, 404);
+
+    if (!verifySignature(JSON.parse(file.data.toString()).order.salt, signature, address))
+      throw new ErrorWithCode("Invalid signature", 403);
+
+    await deleteFile(fileKey);
+
+    res.status(200).json({ message: "Delete request sent" });
   } catch (error) {
     console.error(error);
     handleError("Deleting bid failed", error, res);
